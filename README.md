33d7616d9126fc2087ab306aef3b7b3b53f9645a
EdoFro <35700575+EdoFro@users.noreply.github.com>
2020-11-12 10:52:32 -0300

    Adding documentation

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Freeplane_KeyMap_for_Carnac
  Groovy script to create an yml file containing the actual key shortcuts of Freeplane to be used with Carnac
+
+---
+
+## About Freeplane
+
+[Freeplane](https://www.freeplane.org/wiki/index.php/Home) is a free and open source software application that supports thinking, sharing information and getting things done at work, in school and at home. The software can be used for [mind mapping](https://secure.wikimedia.org/wikipedia/en/wiki/Mind_map) and analyzing the information contained in mind maps. Freeplane runs on any operating system that has a current version of Java installed. It can be run locally or [portably](https://en.wikipedia.org/wiki/Portable_application) from removable storage like a USB drive.
+
+![Freeplane example](https://www.freeplane.org/wiki/images/1/12/FreeplaneWiki.jpg)
+
+---
+
+## About Carnac - 'the Magnificent Keyboard Utility'
+
+[Carnac](http://carnackeys.com/) is a keyboard logging and presentation utility for presentations, screencasts, and to help you become a better keyboard user. (As far as I know it is Windows only)
+
+[Carnac in code52.org](http://code52.org/carnac/)
+
+[Carnac in GitHub](https://github.com/Code52/carnac)
+
+![Carnac screenshot](https://code52.org/carnac/screenshot.png)
+
+![Carnac example](http://carnackeys.com/images/screenshot.gif)
+
+---
+
+## About Freeplane_KeyMap_for_Carnac
+
+The script in this repository takes the information about the key accelerators that are active in **your** Freeplane application and save them as an **.yml** file that can be used by Carnac.
+
+The file is saved in the same folder where the script is.
+
+In the same folder must be copied the **keysDict.txt** file. It is used by the script to translate some keys from the Freeplane denomination to Carnac's.
+
+The information read by the script is the same you can see in Freeplane in the "**Hot keys table**" that you can access through **Help/Key Reference**, which contains standard and custom key combinations.
+
+This new file (Freeplane.yml) has to be then manually copied to the **Carnac\Keymaps** directory
+
+I made the Freeplane.yml file in this repository with this script, so, it contains the key combination I personally use in Freeplane.
+
+## Motivation
+
+I want to do some videos about using some Freeplane features and came to Carnac as an option to show the KeyStrokes I use while using Freeplane.
+
+What I liked about Carnac was that it also shows which action the keystroke is calling (Maybe there are better application for this but I didn't look further).
+
+I made this as a script because key combinations in Freeplane can easily be changed and can be very different between different users.
+
+I'm learning how to do scripts in Groovy for Freeplane and it seamed as a nice challenge to excercise with.
+
+I think it's a script that can be easily adapted for other applications (if needed)
+
+Look at the script itself for further information.
--- get keyMap from Freeplane for Carnac.groovy
+++ get keyMap from Freeplane for Carnac.groovy
@@ -1,23 +1,37 @@
+// @ExecutionModes({ON_SINGLE_NODE})
 
+// texto: string variable that will contain the whole text of the file to be saved
 texto = new StringBuilder()
 texto << "group:          freeplane\nprocess:        javaw\n\n# Reference: Freeplane_KeyMap_for_Carnac\n\nshortcuts:\n"
 
+// menu: defines which menu will be extracted ("main_menu" for all the menus)
 def menu = "main_menu"
+
+// gets the keyStrokes as a MenuEntry Map 
+// http://www.freeplane.org/doc/api/org/freeplane/core/util/MenuUtils.MenuEntry.html
 def keyStrokes = menuUtils.createAcceleratebleMenuEntryTree(menu)
 
+// scriptDir2: the path where this script is saved
 def scriptDir2 = new File(this.class.protectionDomain.codeSource.location.path).parent
-mapa = getMapFromFile("${scriptDir2}/keysDict.txt")
 
+// loads translationMap with pairs in keysDict.txt
+translationMap = getMapFromFile("${scriptDir2}/keysDict.txt")
+
+// executes method wich goes through the keyStrokes MenuEntry Map and appends the information in 'texto'
 caso (keyStrokes)
 
+// saves the information to the freeplane.yml file
 def ymlFile = new File("${scriptDir2}/freeplane.yml")
 ymlFile.text = texto
 
+// it also shows this info in a new node's note
 node.createChild("result").note = texto
 c.statusInfo = "freeplane.yml updated with actual keymap"
 
 
 // ----methods---------------
+// recursivelly goes through the MenuItem tree
+    // it uses the formatCarnac function to get from the Freeplane to the Carnac format
 def caso(n){
     if(n.leaf){
         def menuEntry = n.getUserObject()
@@ -31,30 +45,42 @@ def caso(n){
 }
 
 def formatCarnac(k){
-    def teclas = k.toString().replace("pressed ","").toLowerCase().split(" ")*.capitalize()
+    //gets a list of the keys in the string and format them 
+    def teclas = k.toString().replace("pressed ","").toLowerCase().split(" ")*.capitalize() 
+    //in a new list puts the keys ordered and translated in the Carnac's way 
     def tec2a =  [] << (teclas.size()>=2?teclas.size() == 2?teclas[0]:reordena(teclas[0 .. -2]):[]) << traduce(teclas[-1])
-
-    def c = tec2a.flatten().join("+")
+    //joins all the keys as a single string
+    def c = tec2a.flatten().join("+") 
     return c
 }
 
+// orders the modificators keys ('Ctrl' goes before than 'Alt' goes before than 'Shift')
 def reordena(ts){
     def orden = ['Ctrl','Alt','Shift']
     def mc = { a, b -> orden.indexOf(a) <=> orden.indexOf(b)}
     ts.sort(mc)
 }
 
+//uses translationMap loaded from file to "translate" some of the texts
 def traduce(t){
-    return mapa[t]?:t
+    //elvis operator:
+        //if there is translation -> use it
+        //      else -> use the original string
+    return translationMap[t]?:t
 }
 
+// reads file (line by line) and loads its information in a map
+// ignores comment lines (starting with #)
+// and uses only the one with an "="
 def getMapFromFile(path){
     def file = new File(path)
-    def map = [:]
-    file.eachLine{line-> 
-        if (line.contains('=') && (!line.startsWith("#"))) {        
-            map[line.split('=')[0]] = line.split('=')[1] 
+    def myMap = [:]
+    if (file?.exists()){
+        file.eachLine{line-> 
+            if (line.contains('=') && (!line.startsWith("#"))) {        
+                myMap[line.split('=')[0]] = line.split('=')[1] 
+            } 
         } 
-    } 
-    return map
+    } else { c.statusInfo = "translation file not found"}
+    return myMap
 }
